b46dd7ca8af0512a35164edfb56431656d612530
Paulo Augusto Ponciano <paponciano@gmail.com>
2021-12-15 21:02:08 -0300

    Alterações da 2ª aula

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function carregamentoPagina() {
-    alert("Mensagem apresentada no carregamento do sistema");
-}
+// function carregamentoPagina() {
+//     alert("Mensagem apresentada no carregamento do sistema");
+// }
+
+$(() => {
+    //Código executado no carregamento da página
+    // alert("Mensagem apresentada no carregamento do sistema");
+
+    $("#btnSalvar").click(() => {
+        console.log("FUNÇÃO CLICK FUNCIONOU")
+    })
+})
