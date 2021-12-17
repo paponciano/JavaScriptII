a39722f5d8c70ace4f3790786004c08834d6bb9f
Paulo Augusto Ponciano <paponciano@gmail.com>
2021-12-17 20:57:02 -0300

    Commit final

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
+var editID = null
+
 // function carregamentoPagina() {
 //     alert("Mensagem apresentada no carregamento do sistema");
 // }
+
+function apagarProduto(idProduto) {
+    let result = confirm("Tem a certeza que deseja apagar este produto?")
+
+    if (result) {
+        let listaProdutos = JSON.parse(localStorage.getItem("produtos"))
+
+        for (let i = 0; i < listaProdutos.length; i++) {
+            const { id } = listaProdutos[i]
+            if (id === idProduto) {
+                listaProdutos.splice(i, 1)
+                break;
+            }
+        }
+
+        localStorage.setItem("produtos", JSON.stringify(listaProdutos))
+        populaTabela()
+    }
+}
+
+function editarProduto(idProduto) {
+    let listaProdutos = JSON.parse(localStorage.getItem("produtos"))
+
+    for (let i = 0; i < listaProdutos.length; i++) {
+        const { id } = listaProdutos[i]
+        if (id === idProduto) {
+            editID = idProduto
+            const { nome, quantidade, valor } = listaProdutos[i]
+            $("#nmProduto").val(nome)
+            $("#quantidade").val(quantidade)
+            $("#vlrProduto").val(valor)
+            break;
+        }
+    }
+}
 
 function populaTabela() {
     if (localStorage.getItem("produtos")) {
@@ -12,8 +49,12 @@ function populaTabela() {
                 <td>${produto.nome}</td>
                 <td>${produto.quantidade}</td>
                 <td>${produto.valor}</td>
-                <td style="width: 50px"><button type="button" class="btn btn-info">E</button></td>
-                <td style="width: 50px"><button type="button" class="btn btn-danger">D</button></td>
+                <td style="width: 50px">
+                    <button type="button" class="btn btn-info" onclick="editarProduto(${produto.id})"><i class="fas fa-edit"></i></button>
+                </td>
+                <td style="width: 50px">
+                    <button type="button" class="btn btn-danger" onclick="apagarProduto(${produto.id})"><i class="far fa-trash-alt"></i></button>
+                </td>
             </tr>`)
         })
     }
@@ -24,18 +65,33 @@ $(() => {
     populaTabela()
 
     $("#btnSalvar").click(() => {
-        // let produto = { nome: "", quantidade: 0, valor: 0 }
-        let produto = {}
-        produto.nome = $("#nmProduto").val()
-        produto.quantidade = $("#quantidade").val()
-        produto.valor = $("#vlrProduto").val()
-
         let listaProdutos = []
         if (localStorage.getItem("produtos")) {
             listaProdutos = JSON.parse(localStorage.getItem("produtos"))
         }
 
-        listaProdutos.push(produto)
+        if (!editID) { //ESTADO DE INSERÇÃO
+            // let produto = { nome: "", quantidade: 0, valor: 0 }
+            let produto = {}
+            produto.nome = $("#nmProduto").val()
+            produto.quantidade = $("#quantidade").val()
+            produto.valor = $("#vlrProduto").val()
+
+            produto.id = listaProdutos.length + 1
+            listaProdutos.push(produto)
+        }
+        else { //ESTADO DE EDIÇÃO
+            for (let i = 0; i < listaProdutos.length; i++) {
+                const { id } = listaProdutos[i]
+                if (id === editID) {
+                    listaProdutos[i].nome = $("#nmProduto").val()
+                    listaProdutos[i].quantidade = $("#quantidade").val()
+                    listaProdutos[i].valor = $("#vlrProduto").val()
+                    editID = null
+                    break;
+                }
+            }
+        }
 
         localStorage.setItem("produtos", JSON.stringify(listaProdutos))
 
